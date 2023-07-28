8cfd49b927cdf506dd4532d7152a00e2b493f8d6
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-07-28 15:57:32 -0700

    Update and rename get_genotypes_from_pgen.R to get_all_genotypes.R

---
+++ R/get_all_genotypes.R
@@ -0,0 +1,34 @@
+#' Make a geno_mat
+#' 
+#' @param variants_df dataframe containing all of the variants in the directory, created with get_directory_variants 
+#' @param pgen_list
+#' @keywords get all genotypes
+#' @export
+#' @examples
+#' @export
+#' get_genotypes_from_pgen()
+
+get_all_genotypes <- function(variants_df, pgen_list){
+    variants_df_auto <- filter(variants_df, chromosome != "cX")
+    variants_df_x <- filter(variants_df, chromosome == "cX")
+
+    if (nrow(variants_df_auto) > 0){
+        geno_auto <- get_genotypes(variants_df_auto, directory, psam)
+    } else {
+        geno_auto <- as.data.frame(matrix(ncol = 0, nrow = 0))
+    }
+    if (nrow(variants_df_x) > 0) {
+        geno_x <- get_genotypes(variants_df_x, directory, psam = psam_x)
+    } else {
+        geno_x <- as.data.frame(matrix(ncol = 0, nrow = 0))
+    }
+
+    if (nrow(geno_auto) > 0 & nrow(geno_x) > 0){
+        geno_full <- left_join(geno_auto, geno_x)
+    } else if (nrow(geno_auto) > 0) {
+        geno_full <- geno_auto
+    } else {
+        geno_full <- geno_x
+    }
+    geno_full         
+}
