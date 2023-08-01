b0f292f420f32491c80082168d9636a96fcafc9b
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-01 15:04:10 -0700

    Update example_run_through.R

--- example_run_through.R
+++ example_run_through.R
@@ -5,16 +5,16 @@ library(pgenlibr)
 #devtools::install_github("Lab-Jaiswal/topmedGenotyping")
 library(topmedGenotyping)
 
-directory <- "/o/s/p/topmed/topmed_f9/genotypes/pgen_files/" #for details on the filepath, contact Maggie Maurer
-rsid_fp <- read_tsv("/o/s/p/topmed/topmed_f9/genotypes/pgen_files/chip_snps_vep_annotated_rsid.tsv", col_names = FALSE)
+directory <- "/oak/stanford/projects/topmed/topmed_f9/genotypes/pgen_files/"
+rsid_fp <- read_tsv("/oak/stanford/projects/topmed/topmed_f9/genotypes/pgen_files/chip_snps_vep_annotated_rsid.tsv", col_names = FALSE)
 rsids <- rsid_fp %>% pull(X5)
 
 sequnce <- seq(1:22) %>% append(c("X"))
 chr_list<- sprintf("chr%s", sequnce)  
 
-variants_df <- get_variants(chr_list[21:22], rsids, directory) %>% filter(!is.na(index)) 
+variants_df <- get_variants(chr_list[22:23], rsids, directory) %>% filter(!is.na(index)) 
 
-pgen_list <- get_pgen_list(chr_list[21:22], directory)
+pgen_list <- get_pgen_list(chr_list[22:23], directory)
 
 genotypes <- get_all_genotypes(variants_df, pgen_list, directory)
-write_rds(geno_full, "ukbiobank_genotypes.rda")
+#write_rds(geno_full, "ukbiobank_genotypes.rda")
