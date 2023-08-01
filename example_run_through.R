a3cb4dbbfc0cf12f84f17c8154ea4d4a71212d0f
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2023-08-01 15:50:31 -0700

    Update example_run_through.R

--- example_run_through.R
+++ example_run_through.R
@@ -12,7 +12,7 @@ rsids <- rsid_fp %>% pull(X5)
 sequnce <- seq(1:22) %>% append(c("X"))
 chr_list<- sprintf("chr%s", sequnce)  
 
-variants_df <- get_variants(chr_list[22:23], rsids, directory) %>% filter(!is.na(index)) 
+variants_df <- get_variants(chr_list[23], rsids, directory) %>% filter(!is.na(index)) 
 
 genotypes <- get_all_genotypes(variants_df, directory)
 #write_rds(geno_full, "ukbiobank_genotypes.rda")
